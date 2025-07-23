729f497d4f4c44656f5fa0408f5b23b162ea011c
francyelle349 <francyelle5849@gmail.com>
2025-07-23 16:11:07 -0400

    changes

--- src/home/redesSociais/redes.jsx
+++ src/home/redesSociais/redes.jsx
@@ -1,5 +1,7 @@
 import './redes.css'
 
+import Image from "/woman2.svg"
+
 function Redes(){
     return(<>
     <div className='item'>
@@ -11,19 +13,23 @@ function Redes(){
         
 
           
-          <h3>You Can Reach Out </h3>
+        <div className='subItemTitleSocial'>
+           <h3>You Can Reach Out </h3>
         <div className='socialMedia'>
+
+           
        
         <a href="https://www.linkedin.com/in/francyelle-souza-245968350/"> <i className="uil uil-linkedin" id="link"></i> </a>
         <a href="https://github.com/francyelle349"> <i className="uil uil-github" id="git" ></i> </a>
 
         
         </div>
+        </div>
 
       </div>
 
       <div className='images'>
-        <img src='/woman2.svg' alt="" />
+        <img src={Image} alt="" />
       </div>
 
         
