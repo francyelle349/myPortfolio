c896678331c9775f5c45694f9d8e9228a9609196
francyelle349 <francyelle5849@gmail.com>
2025-07-23 15:24:57 -0400

    deploying

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: 'https://francyelle349.github.io/myPortfolio',
   css: {
     postcss: './postcss.config.js',
   },
